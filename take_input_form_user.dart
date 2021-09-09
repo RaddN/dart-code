69888dc1087c05c18154e20f2df9cc09e10041b7
RaddN <67832195+RaddN@users.noreply.github.com>
2021-09-09 11:53:36 +0600

    Update take_input_form_user.dart

--- take_input_form_user.dart
+++ take_input_form_user.dart
@@ -4,5 +4,14 @@ main() {
 	String name = stdin.readLineSync()!;
 	print("Your name is : $name");
 	
+	
+	print("Movie name");
+	String ?/* question mark is nor null safety*/ movieName = stdin.readLineSync(); 
+	print("Movie director name");
+	String directorName = stdin.readLineSync();
+	print("Movie name is : $movieName , Director name is: $directorName");
+	
+  
+	
   
 }
