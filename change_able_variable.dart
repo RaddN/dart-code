7531737add8d36988cf6524734d30ef31bd52832
RaddN <67832195+RaddN@users.noreply.github.com>
2021-09-09 10:29:52 +0600

    Update change_able_variable.dart

--- change_able_variable.dart
+++ change_able_variable.dart
@@ -1,1 +1,19 @@
-
+main() {
+var name = "Raihan";
+  // name = 25; you can use only sting value. Raihan is string.
+  print(name);
+  //dynamic  means poribortonshil. this is changle able. to any other string,int ect.
+  dynamic nam = "Raihan";
+   nam = 25;
+  print(nam);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
