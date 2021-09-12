7e8e3d08ca0be47bad1e77c66685f6152edd278b
RaddN <67832195+RaddN@users.noreply.github.com>
2021-09-12 10:25:08 +0600

    Create switch_statement.dart

---
+++ switch_statement.dart
@@ -0,0 +1,27 @@
+void main() {
+var marks = "G";
+switch(marks){
+case "A+":
+  print("outstanding");
+  break;
+case "A":
+  print("very good");
+  break;
+case "A-":
+  print("Good");
+  break;
+case "B":
+  print("Low score");
+  break;
+case "C":
+  print("very low score");
+  break;
+case "D":
+  print("very bad");
+  break;
+default:
+  print("You fail");
+  break;
+}
+ 
+}
