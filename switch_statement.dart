8ed382f85ae877b19d766a713887420e13d921d8
RaddN <67832195+RaddN@users.noreply.github.com>
2021-09-12 10:38:36 +0600

    Update switch_statement.dart

--- switch_statement.dart
+++ switch_statement.dart
@@ -1,4 +1,12 @@
 void main() {
+  
+  /* Switch cases use strict comparison (===).
+
+The values must be of the same type to match.
+
+A strict comparison can only be true if the operands are of the same type.
+value muse be same hote hobe. 10 theke boro 20 theke choto arokom type ar kaj switch diye kora jabe na. 
+*/
 var marks = "G";
 switch(marks){
 case "A+":
