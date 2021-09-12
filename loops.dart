ab925e1bb6b504efe101ba4f94ad8c2e46f02567
RaddN <67832195+RaddN@users.noreply.github.com>
2021-09-12 12:40:39 +0600

    Update loops.dart

--- loops.dart
+++ loops.dart
@@ -5,12 +5,20 @@ void main() {
  print("$i Hello");
 }
 }
-// finding even and odd number
+// finding even number
 void main() {
  for(int i = 1; i<=20; i++){
    if(i % 2 == 0) {
      print("even number $i");
    }
-  
+}
+}
+
+// finding odd number 
+void main() {
+ for(int i = 1; i<=20; i++){
+   if(i % 2 == 0) {
+     print("even number $i");
+   }
 }
 }
