c5ac15bac7d385b1ede85b939fcd64a320427cdc
RaddN <67832195+RaddN@users.noreply.github.com>
2021-09-14 12:40:38 +0600

    Update function.dart

--- function.dart
+++ function.dart
@@ -1,10 +1,27 @@
 // function
+import 'dart:io';
 void main(){
   plus();
+  minus();
+  valupass(20,50);
 }
 void plus(){
- int a  = 10;
- int b = 20;
+int a=10 , b=20;
+ var sum = a+b;
+ print("answer");
+ print(sum);
+}
+void minus(){
+int a , b;
+print("input a:");
+a = int.parse(stdin.readLineSync());
+print("input b");
+b = int.parse(stdin.readLineSync());
+  var sub = b-a;
+  print("answer");
+  print(sub);
+}
+void valupass(a , b){
  var sum = a+b;
  print(sum);
 }
