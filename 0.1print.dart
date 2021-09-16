abb5262fa77e9879b42418d3b89bfbcaea328c92
RaddN <67832195+RaddN@users.noreply.github.com>
2021-09-16 12:20:39 +0600

    Update and rename multiple_print.dart to 0.1print.dart

--- 0.1print.dart
+++ 0.1print.dart
@@ -1,4 +1,12 @@
 main() {
+  print("Hello World"); //output hello world
+  
+  // variable value print
+   int a = 2;
+  print(a);
+  
+  
+  // muliple print and variable value print
   var name = "Raihan Hossain", age = 20, study = "CCN", city = "Comilla";
   print(
       "My name is ${name}. I'm ${age} years old.I'm a student of ${study} University. I'm from ${city}.");
