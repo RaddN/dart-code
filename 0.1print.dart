ca44748e19fcb947701a03cd0d0776445b789634
RaddN <67832195+RaddN@users.noreply.github.com>
2021-09-16 14:17:55 +0600

    Update 0.1print.dart

--- 0.1print.dart
+++ 0.1print.dart
@@ -10,4 +10,9 @@ main() {
   var name = "Raihan Hossain", age = 20, study = "CCN", city = "Comilla";
   print(
       "My name is ${name}. I'm ${age} years old.I'm a student of ${study} University. I'm from ${city}.");
+// You can use it also. It will gives you same result.
+print(
+      "My name is ${name}. I'm $age years old.I'm a student of $study University. I'm from $city.");
+
+
 }
