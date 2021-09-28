b174da4d17689ac1a4278df8758940edd8fd536b
RaddN <67832195+RaddN@users.noreply.github.com>
2021-09-28 10:41:49 +0600

    Create 7.0Inheritance.dart

---
+++ 7.0Inheritance.dart
@@ -0,0 +1,14 @@
+void main(){
+	son son1 = new son();
+	son1.bike= "sujuki";
+	print(son1.bike);
+}
+class father{
+	var home;
+	var bike;
+}
+class son extends father{
+  //extends is a inheritance property. akta veriable theke arekti veriable a data access korar jonno inheritance use hoy.
+  // babar somporti sontan pabe
+	
+}
